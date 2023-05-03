2f56086f5e3632c93231e6e9dbe418be1c0c0177
maple8846 <maple8846@users.noreply.github.com>
2023-05-03 16:34:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,3 +12,19 @@ mv dist/$BINARY_NAME .
 
 # Run binary in background with nohup
 nohup ./groupadmin > /dev/null 2>&1 &
+
+# Add the following lines for autostart
+cat << EOF > /etc/systemd/system/groupadmin.service
+[Unit]
+Description=Group Admin
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/nohup $PWD/$BINARY_NAME > /dev/null 2>&1 &
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable groupadmin.service
